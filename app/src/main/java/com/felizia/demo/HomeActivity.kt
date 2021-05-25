49151fba1fcbc711482bc2a40d5dd639ad203bd5
Luis ernesto <luis.alcantara@massive.co>
2021-05-25 22:51:01 +0100

    refactoring

---
+++ app/src/main/java/com/felizia/demo/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.felizia.demo
+
+import android.app.Activity
+import android.os.Bundle
+
+class HomeActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ library/src/main/java/com/javabobo/library/app/debug/MainPageNavigationFragment.kt
@@ -0,0 +1,50 @@
+package com.javabobo.library.app.debug
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.javabobo.library.databinding.FragmentMainPageNavigationBinding
+
+/**
+ * A fragment that user leanback components in kotlin.
+ * Main functionality has been reduced and code refactored.
+ * @author Luis ernesto Alcantara Alcantara.
+ * @date 07/03/2021
+ */
+class MainPageNavigationFragment : BasePageNavigationFragment() {
+
+    private var _binding: FragmentMainPageNavigationBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainPageNavigationBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+
+    }
+
+    private fun initListeners() {
+        binding.browseFrame.onChildFocusListener = mOnChildFocusListener
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+}
